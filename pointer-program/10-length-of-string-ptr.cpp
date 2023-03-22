e369e103d3b77667d494b28e2ffbd2fe91bc42c0
balagithubaccount <balan7122001@gmail.com>
2023-03-22 08:49:59 +0530

    String length without using library function using pointer

---
+++ pointer-program/10-length-of-string-ptr.cpp
@@ -0,0 +1,35 @@
+// Write a program to find the length of a string without using library function.
+
+#include <iostream>
+using namespace std;
+
+int strlength(string s)
+{
+    char *ptr = &s[0];
+    int len = 0;
+    while (*ptr != '\0')
+    {
+        len++;
+        ptr++;
+    }
+    return len;
+}
+int main()
+{
+    string str;
+
+    cout << "Input the string:  ";
+    getline(cin, str);
+    cout << "Length of the string is:  " << strlength(str) << endl;
+
+    return 0;
+}
+
+
+/*
+Sample Output:
+
+Input the string:  School
+Length of the string is:  6
+
+*/
