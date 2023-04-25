c44e7d7699489d261482bc546b6519c81a0b8d63
balagithubaccount <balan7122001@gmail.com>
2023-04-25 08:09:50 +0530

    To find the exponential (e^x) of the given value

---
+++ math-library-function-programs/5-exp-value-math.cpp
@@ -0,0 +1,23 @@
+// Write a Cpp program to find the exponential(e^x) of the given value.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    float giv_value;
+    cout << "Input the value: ";
+    cout << giv_value;
+    cout << "exp(" <<giv_value << ") is:" << exp(giv_value);
+    return 0;
+}
+
+/*
+Sample Output:
+
+Input the value: 3
+exp(3.000000) is: 20.085537
+
+*/
