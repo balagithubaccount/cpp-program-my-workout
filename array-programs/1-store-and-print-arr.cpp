99d2bc1b5d01e318d9072881bee93c591f415057
balagithubaccount <balan7122001@gmail.com>
2023-05-09 11:00:01 +0530

    Store and print array elements

---
+++ array-programs/1-store-and-print-arr.cpp
@@ -0,0 +1,32 @@
+// Write a Cpp program to get array elements store and print
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cout << "Enter the array size: ";
+    cin >> n;
+
+    int array[n];
+
+    cout << "Enter the " << n << " values: ";
+    for (i = 0; i < n; i++)
+        cin >> array[i];
+
+    cout << "You Entered array is: ";
+    for (i = 0; i < n; i++)
+        cout << array[i] << " ";
+    cout << "\n";
+
+    return 0;
+}
+/*
+Sample Output:
+
+Enter the array size: 5
+Enter the 5 values: 5 4 3 1 2
+You Entered array is: 5 4 3 1 2
+
+*/
