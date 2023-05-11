6c71389674e6fe9aea96cc68f27540b4d590c968
balagithubaccount <balan7122001@gmail.com>
2023-05-11 22:30:08 +0530

    To convert a binary number into a decimal number without using array

---
+++ loop-programs/binaryto-decimal.cpp
@@ -0,0 +1,28 @@
+// Write a Cpp program to convert a binary number into a decimal number without using array, function and while loop. Go to the editor
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int num, term, r, dec = 0, base = 1;
+    cout << "Input the binary Number: ";
+    cin >> num;
+    cout << "Decimal number is: ";
+    for (term = num; num != 0; num = num / 10)
+    {
+        r = num % 10;
+        dec = dec + (r * base);
+        base = base * 2;
+    }
+    cout << dec << endl;
+    return 0;
+}
+
+/*
+Sample Output:
+
+Input the binary Number: 1010001
+Decimal number is: 81
+
+*/
