b9a2cddc66ab13a51ab3dee28480ae9bb57a1223
balagithubaccount <balan7122001@gmail.com>
2023-05-16 07:50:16 +0530

    Display the first 10 natural numbers

---
+++ loop-programs/display-10-nat-num.cpp
@@ -0,0 +1,29 @@
+// To display the first 10 natural numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
+
+/*
+Sample Output:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*/
