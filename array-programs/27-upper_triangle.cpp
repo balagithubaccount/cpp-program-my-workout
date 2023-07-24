040ed6b208a1b16995c1dfd5cafac7fe44ee0e4b
balagithubaccount <balan7122001@gmail.com>
2023-07-24 10:32:46 +0530

    To print upper triangular matrix

---
+++ array-programs/27-upper_triangle.cpp
@@ -0,0 +1,59 @@
+// Write a Cpp program To print or display upper triangular matrix.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int size, i, j, zero = 0;
+  cout << "Enter the matrix size: ";
+  cin >> size;
+  int matrix[size][size], uppermat[size][size];
+  cout << "Enter the elements: \n";
+  for (i = 0; i < size; i++)
+  {
+    for (j = 0; j < size; j++)
+    {
+      cin >> matrix[i][j];
+      uppermat[i][j] = matrix[i][j];
+    }
+  }
+  for (i = 0; i < size; i++)
+  {
+    for (j = 0; j < size; j++)
+    {
+      if (j > 0 && i < j)
+        uppermat[i][j] = zero;
+    }
+  }
+  cout << "Upper triangle is: \n";
+  for (i = 0; i < size; i++)
+  {
+    for (j = 0; j < size; j++)
+    {
+      cout << uppermat[i][j] << "\t";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
+
+/*
+Output:
+
+
+Enter the matrix size: 5
+Enter the elements:
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+Upper triangle is:
+5       0       0       0       0
+5       4       0       0       0
+5       4       3       0       0
+5       4       3       2       0
+5       4       3       2       1
+
+*/
