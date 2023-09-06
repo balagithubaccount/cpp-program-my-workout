949c00e8f40ad017278f2424783487aa05c6f8dd
balagithubaccount <balan7122001@gmail.com>
2023-09-06 11:01:09 +0530

    To rearrange the given array so that arr[i] becomes arr[arr[i]]

---
+++ array-programs/90-rearrange-arr[i]-to-arr[arr[i]].cpp
@@ -0,0 +1,41 @@
+// Write a Cpp program to rearrange the given array so that arr[i] becomes arr[arr[i]].......
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int size, i;
+  cout << "Enter the array size: ";
+  cin >> size;
+  int arr[size], sol[size];
+  cout << "Enter the elements only '0' to '" << size - 1 << "':\n";
+  for (i = 0; i < size; i++)
+  {
+    cout << "Element " << i << "-";
+    cin >> arr[i];
+  }
+  cout << "The given array is: ";
+  for (i = 0; i < size; i++)
+    cout << arr[i] << " ";
+
+  for (i = 0; i < size; i++)
+    sol[i] = arr[arr[i]];
+  cout << "\nThe modified array is: ";
+  for (i = 0; i < size; i++)
+    cout << sol[i] << " ";
+  cout << "\n";
+}
+/*
+Output:
+
+Enter the array size: 5
+Enter the elements only '0' to '4':
+Element 0-2
+Element 1-1
+Element 2-4
+Element 3-3
+Element 4-0
+The given array is: 2 1 4 3 0
+The modified array is: 4 1 0 3 2
+*/
