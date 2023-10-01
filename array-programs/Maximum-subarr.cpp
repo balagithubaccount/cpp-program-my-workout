faf5cb11156867cf3bdd464f696c3bd45282e8c2
balagithubaccount <balan7122001@gmail.com>
2023-10-01 12:25:36 +0530

    To find the maximum for each and every contigious subarray of size K from a given array.

---
+++ array-programs/Maximum-subarr.cpp
@@ -0,0 +1,47 @@
+// Write a Cpp program to find the maximum for each and every contigious subarray of size K from a given array.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int size, i, j, len, big;
+   cout << "Enter the array size: ";
+   cin >> size;
+   cout << "Enter the length of each subarray: ";
+   cin >> len;
+   int arr[size], sol[size];
+   cout << "Enter the elements: ";
+   for (i = 0; i < size; i++)
+      cin >> arr[i];
+   for (i = 0; i <= size - len; i++)
+   {
+      big = arr[i];
+      for (j = i; j < len + i; j++)
+      {
+         cout << arr[j] << " ";
+
+         if (big < arr[j])
+            big = arr[j];
+      }
+      sol[i] = big;
+      cout << "----> " << sol[i] << endl;
+   }
+}
+
+/*
+Sample Output:
+
+Enter the array size: 11
+Enter the length of each subarray: 4
+Enter the elements: 1 3 6 21 4 9 12 3 16 10 22
+1 3 6 21 ----> 21
+3 6 21 4 ----> 21
+6 21 4 9 ----> 21
+21 4 9 12 ----> 21
+4 9 12 3 ----> 12
+9 12 3 16 ----> 16
+12 3 16 10 ----> 16
+3 16 10 22 ----> 22
+
+*/
