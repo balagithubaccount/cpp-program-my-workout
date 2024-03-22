3a74a3d6e883a35433c8d3ffb0eac43cee971995
Balasaravanan <120892949+balagithubaccount@users.noreply.github.com>
2024-03-22 17:17:36 +0530

    Add files via upload
    Math library sin() method.

---
+++ math-library-function-programs/sin-method.cpp
@@ -0,0 +1,18 @@
+// This function takes angle (in radian) as an argument and returns its sine value that could be verified using sine curve.
+
+#include <iostream>
+#include <math.h> 
+using namespace std;
+
+int main()
+{
+    double x = 2.3; 
+    cout << "Sine value of x = 2.3: " << sin(x) << endl; 
+    return 0;
+}
+
+/*
+Sample Output:
+
+Sine value of x = 2.3: 0.745705
+*/
