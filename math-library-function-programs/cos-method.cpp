0a4ff56a78803c0af8360d2668edceee82c2c5d7
Balasaravanan <120892949+balagithubaccount@users.noreply.github.com>
2024-03-25 09:44:07 +0530

    Add files via upload
    Cos method in math library function

---
+++ math-library-function-programs/cos-method.cpp
@@ -0,0 +1,19 @@
+// This function takes angle (in radians) as an argument and returns its cosine value that could be verified using cosine curve.
+
+#include <iostream> 
+#include <math.h> 
+using namespace std; 
+
+int main() 
+{ 
+	double x = 2.3; 
+	cout << "Cosine value of x = 2.3: " << cos(x) << endl; 
+
+	return 0; 
+} 
+
+/*
+Sample Output:
+
+Cosine value of x = 2.3: -0.666276
+*/
