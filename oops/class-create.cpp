b89e3f01032f8ff094e75464b14fe2157cb72a29
balagithubaccount <balan7122001@gmail.com>
2024-05-23 16:26:45 +0530

    Oops in Cpp

---
+++ oops/class-create.cpp
@@ -0,0 +1,26 @@
+/*
+Object-oriented programming has several advantages over procedural programming:
+
+    OOP is faster and easier to execute
+    OOP provides a clear structure for the programs
+    OOP helps to keep the C++ code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+    OOP makes it possible to create full reusable applications with less code and shorter development time
+
+*/
+
+// Create a class called "MyClass":
+#include<iostream>
+using namespace std;
+
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+    float myFloat;    // Attribute (float variable)
+};
+
+int main()
+{
+    // This program just for create a class.
+    return 0;
+}
